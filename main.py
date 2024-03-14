99a8e42cff585bf1dd4a99a1a7196b1dd68f816a
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-14 11:28:34 -0300

    Render example app titles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -15,4 +16,14 @@ with col2:
     st.title("Gabriel Henrique Wetzel")
     content = """Image is temporary. I'm not that guy."""
     st.info(content)
-st.write(loremIpsum)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
