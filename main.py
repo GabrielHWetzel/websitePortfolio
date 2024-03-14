ecda660050fe831e434d36ae28686af620790138
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-14 11:07:30 -0300

    Dummy text added

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
+# Todo - remove later
+with open("loremIpsum.txt", 'r') as lp:
+    loremIpsum = lp.read()
+
 with col1:
-    st.image("images/photo.png", width=400)
+    st.image("images/photo.png")
 
 with col2:
     st.title("Gabriel Henrique Wetzel")
     content = """Image is temporary. I'm not that guy."""
     st.info(content)
+st.write(loremIpsum)
